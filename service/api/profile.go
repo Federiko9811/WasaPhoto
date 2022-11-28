96e82c5039401f9eba95bf85b1aa8cf7980321c8
Federiko9811 <fraponi98@gmail.com>
2022-11-28 16:43:24 +0100

    Aggiunti controlli più stringenti sul validator del token e della regex

--- service/api/profile.go
+++ service/api/profile.go
@@ -6,6 +6,7 @@ import (
 	"encoding/json"
 	"github.com/julienschmidt/httprouter"
 	"net/http"
+	"regexp"
 )
 
 func (rt *_router) doLogin(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
@@ -18,20 +19,18 @@ func (rt *_router) doLogin(w http.ResponseWriter, r *http.Request, _ httprouter.
 	if err != nil {
 		rt.baseLogger.Errorf("Decoding Error: %v", err)
 		return
-	} else {
-		var identifier structs.Token
-		identifier.Identifier, err = rt.db.GetUserToken(username.Username)
-		if err != nil {
-			rt.baseLogger.Errorf("Error getting identifier: %v", err)
-			return
-		} else {
-			w.WriteHeader(http.StatusCreated)
-			err = json.NewEncoder(w).Encode(identifier)
-			if err != nil {
-				rt.baseLogger.Errorf("Encoding Error: %v", err)
-				return
-			}
-		}
+	}
+	var identifier structs.Token
+	identifier.Identifier, err = rt.db.GetUserToken(username.Username)
+	if err != nil {
+		rt.baseLogger.Errorf("Error getting identifier: %v", err)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+	err = json.NewEncoder(w).Encode(identifier)
+	if err != nil {
+		rt.baseLogger.Errorf("Encoding Error: %v", err)
+		return
 	}
 }
 
@@ -46,6 +45,18 @@ func (rt *_router) setMyUserName(w http.ResponseWriter, r *http.Request, _ httpr
 		return
 	}
 
+	// check id username respect a regex
+	match, err := regexp.Match(`^[a-zA-Z0-9_-]{3,16}$`, []byte(username.Username))
+	if err != nil || !match {
+		w.WriteHeader(http.StatusBadRequest)
+		res := structs.Message{
+			Message: "Error matching regex: %v",
+		}
+		err = json.NewEncoder(w).Encode(res)
+		utils.ReturnInternalServerError(w, err)
+		return
+	}
+
 	// Get token from header
 	token, err := utils.ExtractToken(r)
 
@@ -57,26 +68,22 @@ func (rt *_router) setMyUserName(w http.ResponseWriter, r *http.Request, _ httpr
 			Message: "Not Valid Token",
 		}
 		err = json.NewEncoder(w).Encode(res)
-		if err != nil {
-			return
-		}
-	} else {
-		// Update username in database for the user with the given token
-		err = rt.db.SetUserName(token, username.Username)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			rt.baseLogger.Errorf("Error setting username: %v", err)
-			return
-		} else {
-			w.WriteHeader(http.StatusOK)
-			res := structs.Message{
-				Message: "Username updated",
-			}
-			err = json.NewEncoder(w).Encode(res)
-			if err != nil {
-				return
-			}
-		}
+		utils.ReturnInternalServerError(w, err)
+		return
 	}
 
+	// Update username in database for the user with the given token
+	err = rt.db.SetUserName(token, username.Username)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		rt.baseLogger.Errorf("Error setting username: %v", err)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	res := structs.Message{
+		Message: "Username updated",
+	}
+	err = json.NewEncoder(w).Encode(res)
+	utils.ReturnInternalServerError(w, err)
+	return
 }
--- service/utils/utils.go
+++ service/utils/utils.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"errors"
 	"net/http"
 	"strconv"
 	"strings"
@@ -8,7 +9,13 @@ import (
 
 func ExtractToken(r *http.Request) (int64, error) {
 	reqToken := r.Header.Get("Authorization")
+	if reqToken == "" {
+		return -1, errors.New("no token found")
+	}
 	splitToken := strings.Split(reqToken, "Bearer ")
+	if len(splitToken) != 2 {
+		return -1, errors.New("no token found")
+	}
 	reqToken = splitToken[1]
 
 	token, err := strconv.ParseInt(reqToken, 10, 64)
@@ -19,4 +26,11 @@ func ExtractToken(r *http.Request) (int64, error) {
 
 	return token, nil
 
+}
+
+// ReturnInternalServerError returns an internal server error if an error is detected
+func ReturnInternalServerError(w http.ResponseWriter, err error) {
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
 }
